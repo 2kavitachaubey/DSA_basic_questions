ec1feed230de49745417ccb074e1260e8fe6c480
2kavitachaubey <chaubeykavita750@gmail.com>
2025-01-29 22:39:43 +0530

    DSA Question

--- src/Pattern.java
+++ src/Pattern.java
@@ -2,6 +2,7 @@ public class Pattern {
     public static void main(String[] args){
 
         //Pattern 1
+        System.out.println("Pattern 1");
         for(int i =0;i<5;i++){
             for(int j=0;j<5;j++){
                 System.out.print('*');
@@ -9,5 +10,22 @@ public class Pattern {
             System.out.println();
         }
 
+        //Pattern 2
+        System.out.println("Pattern 2");
+        for(int i =0;i<5;i++){
+            for(int j=i;j<5;j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+
+        //Pattern 3
+        System.out.println("Pattern 3");
+        for(int i =0;i<5;i++){
+            for(int j=i;j>=0;j--){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
     }
 }
