b1cccf70cf5e0e558822c1e8e26ce322bc4be84b
2kavitachaubey <chaubeykavita750@gmail.com>
2025-01-30 23:22:30 +0530

    DSA Question

--- src/Pattern.java
+++ src/Pattern.java
@@ -1,29 +1,59 @@
 public class Pattern {
     public static void main(String[] args){
 
-        //Pattern 1
-        System.out.println("Pattern 1");
+//        //Pattern 1
+//        System.out.println("Pattern 1");
+//        for(int i =0;i<5;i++){
+//            for(int j=0;j<5;j++){
+//                System.out.print('*');
+//            }
+//            System.out.println();
+//        }
+//
+//        //Pattern 2
+//        System.out.println("Pattern 2");
+//        for(int i =0;i<5;i++){
+//            for(int j=i;j<5;j++){
+//                System.out.print('*');
+//            }
+//            System.out.println();
+//        }
+//
+//        //Pattern 3
+//        System.out.println("Pattern 3");
+//        for(int i =0;i<5;i++){
+//            for(int j=i;j>=0;j--){
+//                System.out.print('*');
+//            }
+//            System.out.println();
+//        }
+//
+//        //Pattern 4
+//        System.out.println("Pattern 4");
+//        for(int i =0;i<5;i++){
+//            for(int j=i;j>=0;j--){
+//                System.out.print('*');
+//            }
+//            System.out.println();
+//        }
+
+        //Pattern 5
+        System.out.println("Pattern 4");
         for(int i =0;i<5;i++){
-            for(int j=0;j<5;j++){
-                System.out.print('*');
+            for(int j=4;j>i;j--){
+                System.out.print(" ");
+            }
+            for(int k=0;k < (i*2)+1 ; k++){
+                System.out.print("*");
             }
             System.out.println();
         }
-
-        //Pattern 2
-        System.out.println("Pattern 2");
-        for(int i =0;i<5;i++){
-            for(int j=i;j<5;j++){
-                System.out.print('*');
+        for(int i =0;i<4;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print(" ");
             }
-            System.out.println();
-        }
-
-        //Pattern 3
-        System.out.println("Pattern 3");
-        for(int i =0;i<5;i++){
-            for(int j=i;j>=0;j--){
-                System.out.print('*');
+            for(int k=8 ; k>(i*2)+1; k--){
+                System.out.print("*");
             }
             System.out.println();
         }
