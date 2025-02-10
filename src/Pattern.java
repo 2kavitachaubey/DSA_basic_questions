9ab1c2731d6ddfff4b051988268f4a4183b7f6f5
2kavitachaubey <chaubeykavita750@gmail.com>
2025-02-10 23:11:23 +0530

    DSA Question

--- src/Pattern.java
+++ src/Pattern.java
@@ -38,24 +38,26 @@ public class Pattern {
 //        }
 
         //Pattern 5
-        System.out.println("Pattern 4");
-        for(int i =0;i<5;i++){
-            for(int j=4;j>i;j--){
-                System.out.print(" ");
-            }
-            for(int k=0;k < (i*2)+1 ; k++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for(int i =0;i<4;i++){
-            for(int j=0;j<i+1;j++){
-                System.out.print(" ");
-            }
-            for(int k=8 ; k>(i*2)+1; k--){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//        System.out.println("Pattern 4");
+//        for(int i =0;i<5;i++){
+//            for(int j=4;j>i;j--){
+//                System.out.print(" ");
+//            }
+//            for(int k=0;k < (i*2)+1 ; k++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i =0;i<4;i++){
+//            for(int j=0;j<i+1;j++){
+//                System.out.print(" ");
+//            }
+//            for(int k=8 ; k>(i*2)+1; k--){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
     }
 }
