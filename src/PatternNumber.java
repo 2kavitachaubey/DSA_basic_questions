c88406e68b00385e7c0fe561a032477f8898b5f2
2kavitachaubey <chaubeykavita750@gmail.com>
2025-02-13 00:59:20 +0530

    DSA Question

--- src/PatternNumber.java
+++ src/PatternNumber.java
@@ -60,15 +60,105 @@ public class PatternNumber {
         System.out.println();
 
 
-        System.out.println("Pattern 3");
+        System.out.println("Pattern 4");
         for(int row = 1; row <= num; row++){
             for(int col = 1; col <= num; col++){
-                if(row==col && (row+col)==num-1){
+                if(row==col && (row+col)==num+1){
                     System.out.print("0 ");
                 }
                 else{
                     System.out.print("1 ");
                 }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        System.out.println("Pattern 5");
+        for(int row = 1; row <= num; row++){
+            for(int col = 1; col <= num; col++){
+                if((row+col)%2 == 0){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        System.out.println("Pattern 6");
+        for(int row = 1; row <= num; row++){
+            for(int col = 1; col <= num; col++){
+                if(col == (num/2 + 1) || row == (num/2 +1)){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        System.out.println("Pattern 7");
+        for(int row = 1; row <= num; row++){
+            for(int col = 1; col <= num; col++){
+                if(col==row || (col+row)==num+1){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        System.out.println("Pattern 8");
+        for(int row = 1; row <= num; row++){
+            for(int col = 1; col <= num; col++){
+                if((row==1  || row ==num) && (col>1 && col<num)){
+                    System.out.print("1 ");
+                }
+                else if((col==1  || col ==num) && (row>1 && row<num)){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        System.out.println("Pattern 9");
+        for(int row = 1; row <= num; row++){
+            for(int col = row; col <= num+row-1; col++){
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        System.out.println("Pattern 10");
+        int n = 1;
+        for(int row = 1; row <= num; row++){
+            for(int col = 1; col <= num; col++) {
+                if(n<10){
+                    System.out.print(n + "  ");
+                }
+                else {
+                    System.out.print(n + " ");
+                }
+                n++;
             }
             System.out.println();
         }
