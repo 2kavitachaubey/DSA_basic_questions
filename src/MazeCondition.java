b2ea116e3c7bf4b74737c4e5b8cbfbbc3fe09f1b
2kavitachaubey <chaubeykavita750@gmail.com>
2025-03-03 23:52:22 +0530

    DSA Question

---
+++ src/MazeCondition.java
@@ -0,0 +1,8 @@
+public class MazeCondition {
+    static void mazeCase(int startingCol, int startingRow, int endCol, int endrow, String value){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
