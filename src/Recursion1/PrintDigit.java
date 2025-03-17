1bf4c901e11934c3aa7dd888586e0c4a39f6335a
2kavitachaubey <chaubeykavita750@gmail.com>
2025-03-17 23:48:31 +0530

    DSA Question

--- src/Recursion1/PrintDigit.java
+++ src/Recursion1/PrintDigit.java
@@ -1,14 +1,14 @@
 package Recursion1;
-
+// 12345
 public class PrintDigit {
     static void printDigit(int num){
         if(num < 1){
             return;
         }
-        printDigit(num - 1);
-        System.out.println(num);
+        printDigit(num/10);
+        System.out.println(num%10);
     }
     public static void main(String[] args) {
-        printDigit(5);
+        printDigit(12345);
     }
 }
